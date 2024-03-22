9e73aac08e4f394e833031ef42d939e6199d7acf
Luban Rahat <153543408+lubanrahat@users.noreply.github.com>
2024-03-23 05:12:39 +0600

    first commit

---
+++ A_Say_Hello_With_C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<algorithm>
+#include<vector>
+#include <cmath>
+#include<climits>
+#include<map>
+#include <set>
+#include<utility>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+
+  string s;
+  cin>>s;
+
+  cout<<"Hello, "<<s<<'\n';
+
+  return 0;
+}
